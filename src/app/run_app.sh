59faa0445b191bc0fb80c1993203f5f198e8f496
mart-r <mart.ratas@gmail.com>
2023-07-27 09:53:04 +0100

    Move to correct environmental variables in the runner script

--- src/app/run_app.sh
+++ src/app/run_app.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Check if the directory specified by MODEL_STORAGE_PATH exists
-if [ ! -d "$MODEL_STORAGE_PATH" ]; then
+if [ ! -d "$MEDCATMLFLOW_MODEL_STORAGE_PATH" ]; then
   # If not, create the directory
-  mkdir -p "$MODEL_STORAGE_PATH"
+  mkdir -p "$MEDCATMLFLOW_MODEL_STORAGE_PATH"
 fi
 
-mkdir -p $LOGS_PATH
+mkdir -p $MEDCATMLFLOW_LOGS_PATH
 
 # Run your application
 cd src
