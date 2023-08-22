7befd56bbda9625e40aacef0d42e53affaf8a190
mart-r <mart.ratas@gmail.com>
2023-08-22 13:03:33 +0100

    Show stats as dict in model metadata

--- src/app/mlflow_integration.py
+++ src/app/mlflow_integration.py
@@ -173,6 +173,10 @@ def get_info(file_path: str) -> dict:
                 and not isinstance(cur_info['performance'], dict)):
             # TODO - make the below better
             cur_info["performance"] = eval(cur_info["performance"])
+        if ('stats' in cur_info
+                and not isinstance(cur_info['stats'], dict)):
+            # TODO - make the below better
+            cur_info["stats"] = eval(cur_info["stats"])
         cur_info['Internal ID'] = _get_run_id(model)
     else:
         cur_info = {"ISSUES": "Metadata not saved",
