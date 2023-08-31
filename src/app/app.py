b6b11efc0ebe2804be9aff7fdb0300e073e1f191
mart-r <mart.ratas@gmail.com>
2023-08-31 10:02:18 +0100

    Removed useless comments

--- src/app/app.py
+++ src/app/app.py
@@ -13,12 +13,9 @@ from .main.views import main_bp
 from .modelmanage.views import models_bp
 from .performance.views import perf_bp
 
-# In docker, this is what we should have
-
 app = Flask(__name__)
 
-# setup logging
-# Create a root logger
+# setup logging for root logger
 logger = logging.getLogger()
 setup_logging(logger)
 
