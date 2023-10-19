f63171486b18a7610881ce80a37b87bf0c5d3505
mart-r <mart.ratas@gmail.com>
2023-10-19 09:09:30 +0100

    Make certain to use Agg backend for matplotlib

--- src/app/performance/imaging.py
+++ src/app/performance/imaging.py
@@ -1,5 +1,7 @@
 from typing import Dict, Iterable, List
 
+import matplotlib
+matplotlib.use('Agg')
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
